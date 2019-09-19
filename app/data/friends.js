1ac88b795c5e0f29bc5683ec64b1eb0c229398e8
Esteban Morell <estebanmorellb@gmail.com>
2019-09-18 22:33:35 -0500

    friendfinder

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,150 +3,64 @@
 var people = [
   {
     name: "Ahmed",
-    photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+    photo:"https://timedotcom.files.wordpress.com/2017/12/joey-degrandis-hsam-memory.jpg",
+   scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
   },
   {
     name: "Jacob Deming",
     photo:
-      "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv8jK9uwi5kud1Qofz0pRUytz5c2H13lYX2PQ5ZEMJTaA9FDT2",
     scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
   },
   {
     name: "Jeremiah Scanlon",
-    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    photo: "https://secure.gravatar.com/avatar/33e390d546778bc346a48b0a9fc0389b?s=400&d=mm&r=g",
     scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
   },
   {
     name: "Louis T. Delia",
     photo:
-      "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+      "https://d1rkab7tlqy5f1.cloudfront.net/LR/Organisatie/Afdelingen/Aerodynamics__Wind_Energy__Flight_Performance_and_Propulsion/Aerodynamics/People/Fulvio_Scarano.jpg",
     scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
   },
   {
     name: "Lou Ritter",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+      "https://d1rkab7tlqy5f1.cloudfront.net/LR/Organisatie/Afdelingen/Aerodynamics__Wind_Energy__Flight_Performance_and_Propulsion/Aerodynamics/People/Ferry_Schrijer.jpg",
     scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
   },
   {
     name: "Jordan Biason",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQd5PQmNRtlCGWkZqW5gAYjWkGePK3iIRIGidoa2BeXLDEkWl38",
     scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
   },
   {
     name: "oscar ",
     photo:
-      "https://images.unsplash.com/photo-1546538994-4f15d0aa966f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+      "https://d1rkab7tlqy5f1.cloudfront.net/LR/Organisatie/Afdelingen/Aerodynamics__Wind_Energy__Flight_Performance_and_Propulsion/Aerodynamics/People/M_Schmelzer.jpg",
     scores: ["3", "2", "3", "3", "3", "3", "3", "3", "3", "4"]
   },
   {
     name: "danyel",
     photo:
-      "https://www.facebook.com/photo.php?fbid=2127669724229121&set=a.1389367394726028&type=3&theater",
+      "https://pbs.twimg.com/profile_images/417046827943591936/m5T1WY4g.jpeg",
     scores: ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2"]
   },
   {
-    name: "d",
-    photo: "d",
+    name: "Jessica R.",
+    photo: "http://blogs.vmware.com/careers/files/2015/10/Madison-Soave-Headshot.jpg",
     scores: ["3", "2", "2", "2", "2", "3", "2", "3", "2", "2"]
   },
   {
-    name: "d",
-    photo: "d",
+    name: "Valery M.",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5PI_XHSO6kKjAgeRyS5mvMgB7H-Hbdhxn84xbIi-YLIToxrvv",
     scores: ["3", "2", "2", "2", "2", "3", "2", "3", "2", "2"]
   },
   {
-    name: "d",
-    photo: "d",
+    name: "Anne S.",
+    photo: "https://www.israeliamerican.org/sites/default/files/styles/circular-headshot/public/team-headshots/image_0.jpg?itok=amfJOYl4",
     scores: ["3", "2", "2", "2", "2", "3", "4", "3", "2", "2"]
-  },
-  {
-    name: "d",
-    photo: "d",
-    scores: ["1", "2", "2", "2", "2", "3", "4", "3", "2", "2"]
-  },
-  {
-    name: "Juan",
-    photo: "no have",
-    scores: ["4", "3", "3", "3", "4", "3", "3", "3", "3", "3"]
-  },
-  {
-    name: "Juan",
-    photo: "no have",
-    scores: ["4", "3", "3", "3", "4", "3", "3", "3", "3", "3"]
-  },
-  {
-    name: "BarbieDoLLxo",
-    photo: "kdsliijewjej",
-    scores: ["5", "5", "4", "2", "3", "1", "5", "3", "4", "3"]
-  },
-  {
-    name: "Juan",
-    photo: "no have",
-    scores: ["4", "3", "3", "3", "4", "3", "3", "3", "3", "3"]
-  },
-  {
-    name: "Juancho",
-    photo: "no have",
-    scores: ["4", "3", "3", "3", "4", "3", "3", "3", "3", "3"]
-  },
-  {
-    name: "Juancho",
-    photo: "no have",
-    scores: ["2", "5", "5", "5", "1", "5", "4", "2", "4", "2"]
-  },
-  {
-    name: "patrick",
-    photo: "no photo",
-    scores: ["3", "3", "4", "2", "3", "3", "3", "4", "2", "2"]
-  },
-  {
-    name: "Joe",
-    photo: "urlfdsjf",
-    scores: ["3", "4", "3", "2", "3", "3", "3", "4", "5", "4"]
-  },
-  {
-    name: "Elizabeth",
-    photo: "facebook.com",
-    scores: ["1", "2", "3", "1", "3", "2", "1", "3", "1", "2"]
-  },
-  {
-    name: "Elizabeth",
-    photo: "facebook.com",
-    scores: ["1", "2", "3", "1", "3", "2", "1", "3", "1", "2"]
-  },
-  {
-    name: "Elizabeth",
-    photo: "facebook.com",
-    scores: ["1", "2", "3", "1", "3", "2", "1", "3", "1", "2"]
-  },
-  {
-    name: "Elizabeth",
-    photo: "facebook.com",
-    scores: ["1", "2", "3", "1", "3", "2", "1", "3", "1", "2"]
-  },
-  {
-    name: "aaaa",
-    photo: "jhbjhbjhb",
-    scores: ["3", "3", "3", "2", "3", "4", "4", "5", "3", "5"]
-  },
-  {
-    name: "me",
-    photo: "no",
-    scores: ["3", "3", "5", "3", "1", "2", "4", "4", "2", "3"]
-  },
-  {
-    name: "me",
-    photo: "no",
-    scores: ["3", "3", "5", "3", "1", "2", "4", "4", "2", "3"]
-  },
-  {
-    name: "rvwsd",
-    photo: "dd",
-    scores: ["2", "2", "2", "2", "2", "2", "2", "2", "2", "3"]
   }
 ];
 
